eb5e122add481c723e1f78283e3fe123737e6a77
Kaitlyn Jandrucko <KaitlynJandrucko@kaitlyns-macbook-pro.local>
2019-03-28 10:30:29 -0400

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Kaitlyn Jandrucko on 3/28/19.
+//  Copyright © 2019 Kaitlyn Jandrucko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
